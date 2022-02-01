5bdc51ae7f1983490f29e5c4c9133ca4fdfed80d
wenkey <muni.v.g@everest.engineering>
2022-02-01 23:40:49 +0530

    Initial commit

---
+++ src/main/java/com/everest/employeeportal/EmployeeportalApplication.java
@@ -0,0 +1,13 @@
+package com.everest.employeeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeportalApplication.class, args);
+	}
+
+}
