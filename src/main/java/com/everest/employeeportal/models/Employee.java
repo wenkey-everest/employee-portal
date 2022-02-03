3ceee237a90b0d177f532c40f3e9133081c4a5cd
wenkey <muni.v.g@everest.engineering>
2022-02-03 10:57:42 +0530

    Added remaining details of employee

--- src/main/java/com/everest/employeeportal/models/Employee.java
+++ src/main/java/com/everest/employeeportal/models/Employee.java
@@ -12,15 +12,15 @@ import java.time.LocalDate;
 public class Employee {
     private Long empID;
     private String name;
-//    private String everestEmailId;
-//    private String password;
-//    private String personalEmailId;
-//    private LocalDate dateOfBirth;
-//    private LocalDate dateOfJoin;
-//    private String designation;
-//    private int experience;
-//    private String bio;
-//    private Address presentAddress;
-//    private Address permanentAddress;
+    private String everestEmailId;
+    private String password;
+    private String personalEmailId;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfJoin;
+    private String designation;
+    private int experience;
+    private String bio;
+    private Address presentAddress;
+    private Address permanentAddress;
 
 }
