ea874ebd305afd4e5d12007eb702e3e4d2151ec9
wenkey <muni.v.g@everest.engineering>
2022-02-07 21:13:40 +0530

    enabled auto config

--- src/main/java/com/everest/employeeportal/EmployeeportalApplication.java
+++ src/main/java/com/everest/employeeportal/EmployeeportalApplication.java
@@ -1,9 +1,12 @@
 package com.everest.employeeportal;
 
+import com.everest.employeeportal.config.DbConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DbConfig.class)
 public class EmployeeportalApplication {
 
 	public static void main(String[] args) {
