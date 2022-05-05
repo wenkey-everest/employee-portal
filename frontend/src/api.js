9a43e9959dc0e3f1cb66da986c57646b9c56f4ed
wenkey <muni.v.g@everest.engineering>
2022-05-05 13:19:28 +0530

    Updated IP address from local host

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:8080'
+const API_BASE_URL = 'http://52.12.90.48:8080'
 
 export function fetchEmployees() {
     return axios.get(`${API_BASE_URL}/api/employees`);
