874e71a279bad6e2c0af62534cbbebd47d4d3ed2
wenkey <muni.v.g@everest.engineering>
2022-05-18 15:25:29 +0530

    Migrated to cloud to secure secrets

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,20 +1,20 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "3.26.0"
-#     }
-#   }
-#   required_version = ">= 1.1.0"
+ terraform {
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "3.26.0"
+     }
+   }
+   required_version = ">= 1.1.0"
 
-#   cloud {
-#     organization = "api-login"
+   cloud {
+     organization = "api-login"
 
-#     workspaces {
-#       name = "employee-portal"
-#     }
-#   }
-# }
+     workspaces {
+       name = "employee-portal"
+     }
+   }
+ }
 
 variable "AWS_REGION" {
   default     = "us-west-2"
