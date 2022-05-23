40287e72549d72fc81cb3bacc79c56ec8e46f777
wenkey <muni.v.g@everest.engineering>
2022-05-23 19:19:38 +0530

    Modified environmental variables based on react

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const ENVIRONMENT_IP = process.env.PUBLIC_IP;
-
-const API_BASE_URL = "http://" + process.env.PUBLIC_IP + ":8080";
+const API_BASE_URL = "http://" + process.env.REACT_APP_PUBLIC_IP + ":8080";
 
 export function fetchEmployees() {
     return axios.get(`${API_BASE_URL}/api/employees`);
